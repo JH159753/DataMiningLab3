6a2029517e61473caa5c7b4c757aa51d2b58c96c
JH159753 <JHL159753@gmail.com>
2022-04-11 19:17:26 -0700

    implemented n and eps

--- clustering.py
+++ clustering.py
@@ -38,66 +38,106 @@ def lloyds(data, k, columns, centers=None, n=None, eps=None):
             clusters.append([])
 
         #not one time things here
+        iterations = 0
 
-        #for each row in the data, do this
-        for i in range(len(data)):
+        while (iterations < n):
             
-            closestCluster = 0
-            closestClusterDistance = 100
-            #for each center, do this
-            for j in range(len(centers)):
-                #calculate distance between this point and the center
+            iterations = iterations + 1
+            print("Iteration "  + str(iterations))
+            #declare old center 
+            oldCenters = []
+            for i in range(len(centers)):
+                oldCenters.append([])
+                for j in range(len(centers[i])):
+                    oldCenters[i].append([j])
+
+            #copy data into oldCenters by value
+            for i in range(len(centers)):
+                for j in range(len(centers[i])):
+                    oldCenters[i][j] = centers[i][j]
+
+            print (oldCenters)
+
+            #for each row in the data, do this
+            for i in range(len(data)):
                 
-                #reset the closestDistance
-                currentDistance = 0
-                #For each column, do this
-                currentColumn = 0
-                for column in columns:
-
-                    currentDistance = currentDistance + (data.loc[i][column] - centers[j][currentColumn])**2
-                    currentColumn = currentColumn + 1
+                closestCluster = 0
+                closestClusterDistance = math.inf
+                #for each center, do this
+                for j in range(len(centers)):
+                    #calculate distance between this point and the center
                     
-                #after adding all these together, square root the value
-                currentDistance = currentDistance ** (1/2)
+                    #reset the closestDistance
+                    currentDistance = 0
+                    #For each column, do this
+                    currentColumn = 0
+                    for column in columns:
+
+                        currentDistance = currentDistance + (data.loc[i][column] - centers[j][currentColumn])**2
+                        currentColumn = currentColumn + 1
+                        
+                    #after adding all these together, square root the value
+                    currentDistance = currentDistance ** (1/2)
+                    
+
+                    #if the currentDistance calculated here is less than the minimum, the data point is assigned to this one
+                    if currentDistance < closestClusterDistance:
+                        closestCluster = j
+                        closestClusterDistance = currentDistance
+                        
+                        
+
+                #append the point to whichever cluster is closest
+                clusters[closestCluster].append(data.loc[i])
+
+            #Now that we have our rows placed in clusters, calculate new cluster center
+            #reset center's values
+            for i in range(len(centers)):
+                for j in range(len(centers[i])):
+                    centers[i][j] = 0
+
+
+
+            #for each cluster, do this
+            for i in range(len(clusters)):
+                #for each row in a cluster, do this
+                for j in range(len(clusters[i])):
+                    #for each column we care about in the row, do this
+                    currentColumn = 0
+                    for column in columns:
+                        centers[i][currentColumn] = centers[i][currentColumn] + clusters[i][j][column]
+                        currentColumn = currentColumn + 1
+                    
                 
-
-                #if the currentDistance calculated here is less than the minimum, the data point is assigned to this one
-                if currentDistance < closestClusterDistance:
-                    closestCluster = j
-                    closestClusterDistance = currentDistance
+            #after adding everything up, divide by the length of the clusters to get the average
                     
-                    
+            for i in range(len(centers)):
+                for j in range(len(centers[i])):
+                    centers[i][j] = centers[i][j] / len(clusters[i])
 
-            #append the point to whichever cluster is closest
-            clusters[closestCluster].append(data.loc[i])
+            print (centers)
 
-        #Now that we have our rows placed in clusters, calculate new cluster center
-        #reset center's values
-        for i in range(len(centers)):
-            for j in range(len(centers[i])):
-                centers[i][j] = 0
+            #compare old center to this if eps is not none
+            if (eps is not None):
+                minDistanceMoved = math.inf
+                distanceMoved = 0
+                for i in range(len(centers)):
+                    for j in range(len(centers[i])):
+                        distanceMoved = distanceMoved + (centers[i][j] - oldCenters[i][j])**2 
+                    distanceMoved = distanceMoved**(1/2)
+                    print(distanceMoved)
 
-        print (centers)
+                    if (distanceMoved < minDistanceMoved):
+                        minDistanceMoved = distanceMoved
+                        
 
-        #for each cluster, do this
-        for i in range(len(clusters)):
-            #for each row in a cluster, do this
-            for j in range(len(clusters[i])):
-                #for each column we care about in the row, do this
-                currentColumn = 0
-                for column in columns:
-                    centers[i][currentColumn] = centers[i][currentColumn] + clusters[i][j][column]
-                    currentColumn = currentColumn + 1
-                
+
+                if (distanceMoved < eps):
+                    break
+
+
+
             
-        #after adding everything up, divide by the length of the clusters to get the average
-                
-        for i in range(len(centers)):
-            for j in range(len(centers[i])):
-                centers[i][j] = centers[i][j] / len(clusters[i])
-
-        print (centers)
-
         
         
             
@@ -117,7 +157,7 @@ def main():
     #import the data from the csv
     data = pd.read_csv("testdata.csv")
     columns = [1, 2, 3, 4]
-    lloyds(data, 2, columns, None, 10, None)
+    lloyds(data, 2, columns, None, 10, .1)
 
 if __name__ == '__main__':
     main()
